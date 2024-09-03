61b92acba24232c0b00002bfc9ec7cec785a64e0
Седов Дмитрий <d555601@gmail.com>
2024-09-03 17:45:11 +0300

    добавили три метода фильтрацреализовали - filter, computed, watch

--- Chapter02/Exercise2.06/src/components/Exercise2-06.vue
+++ Chapter02/Exercise2.06/src/components/Exercise2-06.vue
@@ -1,91 +1,71 @@
 <template>
   <div class="container">
-    <h1>Methods vs watchers vs computed props</h1>
-
+    <h1>Method vs watchers vs computed props</h1>
     <div class="col">
       <input
         type="text"
-        placeholder="Search with method"
+        placeholder="Search method"
         v-model="input"
         @keyup="searchMethod"
       />
-
       <ul>
         <li v-for="(item, i) in methodFilterList" :key="i">{{ item }}</li>
       </ul>
     </div>
-
     <div class="col">
       <input type="text" placeholder="Search with computed" v-model="input2" />
-
       <ul>
         <li v-for="(item, i) in computedList" :key="i">{{ item }}</li>
       </ul>
     </div>
-
     <div class="col">
-      <input type="text" placeholder="Search with watcher" v-model="input3" />
-
+      <input type="text" placeholder="Search with watch" v-model="input3" />
       <ul>
         <li v-for="(item, i) in watchFilterList" :key="i">{{ item }}</li>
       </ul>
-    </div>
   </div>
+</div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      // Shared
-      frameworkList: [
-        "Vue",
-        "React",
-        "Backbone",
-        "Ember",
-        "Knockout",
-        "jQuery",
-        "Angular",
-      ],
+<script setup>
+import { ref, computed, watch } from "vue";
 
-      // Method
-      input: "",
-      methodFilterList: [],
-      // Computed
-      input2: "",
-      // Watcher
-      input3: "",
-      watchFilterList: [],
-    };
-  },
-  created() {
-    this.searchMethod();
-  },
-  watch: {
-    input3: {
-      handler() {
-        this.watchFilterList = this.frameworkList.filter((item) =>
-          item.toLowerCase().includes(this.input3.toLowerCase())
-        );
-      },
-      immediate: true,
-    },
-  },
-  computed: {
-    computedList() {
-      return this.frameworkList.filter((item) => {
-        return item.toLowerCase().includes(this.input2.toLowerCase());
-      });
-    },
-  },
-  methods: {
-    searchMethod() {
-      this.methodFilterList = this.frameworkList.filter((item) =>
-        item.toLowerCase().includes(this.input.toLowerCase())
-      );
-    },
-  },
+// Shared
+const frameworkList = [
+  "Vue",
+  "React",
+  "Backbone",
+  "Ember",
+  "Knockout",
+  "jQuery",
+  "Angular",
+];
+
+// Method
+const input = ref("");
+const methodFilterList = ref(frameworkList);
+const searchMethod = () => {
+  methodFilterList.value = frameworkList.filter((item) =>
+    item.toLowerCase().includes(input.value.toLowerCase())
+  );
 };
+
+// Computed
+const input2 = ref("");
+const computedList = computed(() => {
+  //  используем вычисляемое свойство, чтобы автоматически пересчитывать отфильтрованный список при изменении значения input2 в поле ввода.
+  return frameworkList.filter((item) =>
+    item.toLowerCase().includes(input2.value.toLowerCase())
+  );
+});
+// Watch - для реактивного отслеживания изменений в определенных данных. В данном случае она используется для отслеживания изменений в input3. Каждый раз, когда значение input3 изменяется, вызывается функция, переданная вторым аргументом в watch.
+const input3 = ref("");
+const watchFilterList = ref([...frameworkList]); // переменная хранит фильтрованный список элементов. Изначально она содержит копию исходного списка frameworkList. Поскольку она объявлена с помощью ref, её изменения также реактивно отслеживаются Vue.
+watch(input3, (newValue)=>{ // Внутри функции watch, используется метод массива .filter(), чтобы создать новый массив, содержащий только те элементы frameworkList, которые соответствуют критерию поиска. Критерий поиска - наличие подстроки newValue, преобразованной к нижнему регистру, в каждом элементе списка, который также преобразуется к нижнему регистру.
+  watchFilterList.value= frameworkList.filter((item) => item.toLocaleLowerCase().includes(newValue.toLocaleLowerCase()));
+});
+
+
 </script>
 
 <style scoped>
@@ -95,11 +75,13 @@ export default {
   max-width: 600px;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
+
 .col {
   width: 33%;
   height: 100%;
   float: left;
 }
+
 input {
   padding: 10px 6px;
   margin: 20px 10px 10px 0;
